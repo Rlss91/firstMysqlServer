1ac1e622b9a1ad0f55b994ba98172fcaa6070a04
rlss91 <rlss91@gmail.com>
2022-01-18 11:47:32 +0200

    config

--- model/mysqlpool.js
+++ model/mysqlpool.js
@@ -1,13 +1,21 @@
+const config = require("config");
 const mysql = require("mysql2");
 
 let pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "eshop",
+  ...config.get("dbConfig"),
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
+
+// let pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "1234",
+//   database: "eshop",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
 
 module.exports.pool = pool.promise();
